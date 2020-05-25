e4d8b5de2fdd2c5781d09e8d40af38838b462e51
Felix Feraud <felix.feraud@gmail.com>
2020-05-25 18:57:40 +0200

    Fine-tuning executable code added.

--- src/fine_tuning.py
+++ src/fine_tuning.py
@@ -1,30 +1,28 @@
-from keras import callbacks
-from keras import optimizers
-from keras.applications import resnet
+import tensorflow as tf
+import keras
+import os
+import shutil
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
 
-from func_utils import setup_data_generators, freeze_n_layers, build_tl_model, train_model
-from layer_builders import basic_layers_builder
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications import resnet, vgg19, NASNetLarge
+
+from keras.preprocessing import image
+from keras.models import Model, Sequential, load_model
+from keras.layers import Dense, GlobalAveragePooling2D, GlobalMaxPooling2D, Lambda, Dropout, Input, BatchNormalization, Flatten
+from keras.constraints import maxnorm
+from keras.regularizers import l2, l1
+from keras import callbacks
+from keras import backend as K
+
+from keras import optimizers
 
 data_path = 'drive/My Drive/TER/'
 
-image_size = (224, 224)
-train_gen, val_gen = setup_data_generators(data_path, image_size, 32, resnet.preprocess_input)
+print('lol')
 
-base_model = resnet.ResNet50(weights='imagenet', include_top=False, input_shape=(*image_size, 3))
-freeze_n_layers(base_model, len(base_model.layers))
-
-model = build_tl_model(base_model, basic_layers_builder, image_size, 200)
-
-early_stop_tl = callbacks.EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=10, min_delta=0.01)
-early_stop_ft = callbacks.EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=15, min_delta=0.01)
-opt_tl = optimizers.Adam(lr=1e-3)
-opt_ft = optimizers.Adam(lr=1e-6)
-
-train_model(model, train_gen, val_gen, 15, opt_tl, callbacks=[early_stop_tl])
-
-freeze_n_layers(base_model, 81)
-
-
-train_model(model, train_gen, val_gen, 15, opt_ft, callbacks=[early_stop_ft])
-
-model.save(data_path + 'models/test.h5')
+print(tf.__version__)
